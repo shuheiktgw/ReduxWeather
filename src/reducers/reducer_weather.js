6332dabb5514cc35bd85031652d8df25f4459515
shuheiktgw <shuhei.kitagawa@gmail.com>
2016-10-13 08:20:22 +0900

    Finished building the weather app

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,6 +1,12 @@
+import {FETCH_WEATHER} from '../actions/index';
+
 /**
  * Created by Shuhei on 2016/10/10.
  */
-export default function(state = null, action){
+export default function(state = [], action){
+    switch (action.type){
+        case  FETCH_WEATHER:
+            return [action.payload.data, ...state];
+    }
     return state;
 }
